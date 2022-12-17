e921bb8235720bf2e7d327d3b5746adf208bf6fa
MelnykY <106350059+MelnykY@users.noreply.github.com>
2022-12-17 01:40:10 +0100

    1st task - done

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from "./gallery-items.js";
+
+const gallery = document.querySelector(".gallery");
+const imageEl = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+    <img class="gallery__image"
+    src ='${preview}' 
+    alt ='${description}' 
+    data-source='${original}'
+    />
+  </a>
+</div>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML('beforeend', imageEl);
+
+gallery.addEventListener("click", onImageClick);
+
+function onImageClick(event) {
+  event.preventDefault();
+
+  
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`
+  );
+  instance.show();
+
+  document.addEventListener("keydown", event => {
+    if (event.key === "Escape") instance.close();
+  });
+}
